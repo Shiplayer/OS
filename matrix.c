80946896c4645143fbc672e3d814d87489ff7b2b
Shiplayer <lllytep199515@yandex.ru>
2016-04-15 10:20:17 +0300

    matrix_v002

--- matrix.c
+++ matrix.c
@@ -6,10 +6,79 @@
 #include <sys/types.h>
 
 
-key_t key = 5223;
-size_t size = sizeof(int);
+key_t key;
+size_t size;
+key_t key2;
+size_t size2;
 
 int main(){
+	key = ftok("matrix", 1);
+	key = ftok("matrix2", 2);
+	pid_t pid;
+	while(1){
+		if((pid = fork()) == 0){
+			int m, n;
+			scanf("%d %d", &m, &n);
+			int *matrix = malloc(m * n * sizeof(int));
+			for(int i = 0; i < m; i++){
+				for(int j = 0; j < n; j++)
+					scanf("%d", matrix[i + j * m]);
+			}
+			size = m * n * sizeof(int);
+			int shm_number = shmget(key, size, IPC_CREAT | 0666);
+			if(shm_number < 0) {
+				perror("shmget");
+				exit(1);
+			}
+			int *shm = shmat(shm_number, NULL, 0);
+			if(shm == (int *) -1){
+				perror("shmat");
+				exit(1);
+			}
+			int* s = shm;
+			for(int i = 0; i < m; i++){
+				for(int j = 0; j < n; j++){
+					*s++ = matrix[i + j * m];
+				}
+			}
+			scanf("%d %d", &m, &n);
+			size2 = m * n * sizeof(int);
+			int shm_number2 = shmget(key, size, IPC_CREAT | 0666);
+			if(shm_number2 < 0){
+				perror("shmget2");
+				exit(1);
+			}
+			int *shm2 = shmat(shm_number, NULL, 0);
+			if(shm2 == (int *) -1){
+				perror("shmat2");
+				exit(1);
+			}
+			int* s = shm2;
+			for(int i = 0; i < m; i++){
+				for(int j = 0; j < n; j++){
+					*s++ = matrix[i + j * m];
+				}
+			}
+			pid = fork();
+			if(pid == 0){
+				int 
+			} else if(pid != -1)
+				exit(0);
+			else{
+				perror("fork2");
+				exit(1);
+			}
+		} else if(pid != -1){
+			wait(1);
+		} else{
+			perror("fork");
+			exit(1);
+		}
+	}
+	return 0;
+}
+
+/*int main(){
 	pid_t pid = fork();
 	if(pid == 0){
 		printf("child\n");
@@ -56,4 +125,4 @@ int main(){
 		exit(1);
 	}
 	exit(0);
-}
+}*/
